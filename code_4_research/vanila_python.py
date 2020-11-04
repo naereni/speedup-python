710cf328780a69c46dbf3d8363c23b0c1e070354
NeonDaisy <edvard@iwebstudio.ru>
2020-11-04 22:53:56 +0300

    add new data

--- code_4_research/vanila_python.py
+++ code_4_research/vanila_python.py
@@ -1,34 +1,59 @@
 ### main file of vanila python
-import time
-# fibonacci numbers
+
+import cProfile
+import random
+
+
+# fibonacci number with index n
 def fib(n: int):
     if n < 3:
         return 1
     return fib(n-1) + fib(n-2)
 
-# check prime number
-def is_prime(n: int):
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
 
-# list of prime num
+# list of Prime numbers up to a number n
 def list_prime_num(n: int):
+    # check 
+    def is_prime(n: int):
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+
     res_list = []
     for num in range(1, n):
         if is_prime(num):
             res_list.append(num)
     return res_list
 
-# factorial 
-def fac(n):
+
+# factorial
+def fac(n: int):
     p = 1
     for i in range(2, n+1):
         p *= i
     return p
 
-start = time.time()
-fac(100000)
-end = time.time()
-print(end - start)
+
+# auxiliary function for sorting algorithm
+def array_nums(length: int):
+    raw_nums = []
+    for i in range(1, length):
+        raw_nums.append(random.randint(0, 1000000))
+    return raw_nums
+
+
+# yes yes the most stupid version of bubble sort
+def bubble_sort(nums: list):
+    for i in range(len(nums)):
+        for j in range(len(nums)-i-1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
+
+
+# cProfile.run('fib(40)')
+# cProfile.run('list_prime_num(5000)')
+# cProfile.run('fac(100000)')
+
+cProfile.run('bubble_sort(array_nums(10000))')
